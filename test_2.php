d4288cecc4359d439d87cda1ecf7c00820a4ae39
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2023-06-12 23:25:35 +0100

    Test 5b

--- test_2.php
+++ test_2.php
@@ -3,5 +3,6 @@ echo "This is test 2";
 echo "<br>This is test 3; a commit that we are expecting to see in test domain";
 echo "<br>This is test 4. The previous one did not run on manual deployment but worked on test deployment.";
 echo "<br>Test 5: I will make changes and then deploy to test site";
+echo "<br>5b: Further changes";
 
 ?>
