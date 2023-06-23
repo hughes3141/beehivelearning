dd3148ae6ec73bcf7dc91811534a3af4424bc099
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2023-06-23 13:46:44 +0100

    A few changes to header and footer: changing font for header, removing freepic.com banner at bottom of footer

--- header_tailwind.php
+++ header_tailwind.php
@@ -52,7 +52,7 @@
     <nav id="header" class="fixed w-full z-30 top-0 text-white">
       <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div class="pl-4 flex items-center">
-          <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+          <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl font-mono" href="#">
             
           <svg class="h-8 fill-current inline" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
             <g>
