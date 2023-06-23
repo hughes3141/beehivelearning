1f009e6b20e5273a6bdd3ce05e7e0e489e8944f3
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2023-06-23 12:26:57 +0100

    Dropdown menu button is now working

--- header_tailwind.php
+++ header_tailwind.php
@@ -14,8 +14,10 @@
 
     <!-- Use below for development
     
-   --> 
+
     <script src="https://cdn.tailwindcss.com"></script>
+   --> 
+    <link rel="stylesheet" href="/dist/output.css"/>
       
 
 
@@ -97,11 +99,30 @@
             </li>
           </ul>
           <button
-            id="navAction"
-            class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+            id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+            class="flex items-center mx-auto lg:mx-0 hover:underline bg-pink-200 lg:bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
           >
             Action
           </button>
+          <div id="dropdownNavbar" class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-800 dark:divide-gray-600">
+                <ul class="py-1 text-sm text-gray-800 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                  <li>
+                    <a href="/user/user3.0.php" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                  </li>
+                  <!--
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                  </li>
+                  
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                  </li>
+                  -->
+                </ul>
+                <div class="py-1">
+                  <a href="/signout.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Sign out</a>
+                </div>
+            </div>
         </div>
       </div>
       <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
